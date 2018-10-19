5c6ca27842bb6daf76b8d829f146b1f381063920
F0urchette <mario.32@live.fr>
2018-10-19 17:45:11 +0200

    presque fini

---
+++ src/test/java/deqo/mpon/mysimplestack/ItemTest.java
@@ -0,0 +1,37 @@
+package deqo.mpon.mysimplestack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+    private Item item;
+
+    @Before
+    public void testSetUp() {
+        this.item = new Item();
+    }
+
+    @Test
+    public void testGetValeur() {
+        assertEquals(0, this.item.getValeur());
+    }
+
+    @Test
+    public void testSetValeur() {
+        this.item.setValeur(54);
+        assertEquals(54, this.item.getValeur());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("0", this.item.toString());
+    }
+
+    @Test
+    public void testInitItem() {
+        this.item = new Item(5);
+        assertEquals(5, item.getValeur());
+    }
+}
